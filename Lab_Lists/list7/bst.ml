6f87f153b4636b6d4a6c85ef2dd5933a6cc549f1
Bartosz <bartekfigiel98@gmail.com>
2018-11-20 21:29:10 +0100

    Finished implementation still doesn't work

--- Lab_Lists/list7/bst.ml
+++ Lab_Lists/list7/bst.ml
@@ -1,52 +1,73 @@
+
 type 'a bst = Leaf | Node of 'a * 'a bst * 'a bst
 
 type 'a tr = {mutable root: int bst}
 
 exception NotFound of string
 
-let create = {root = Leaf}
+let create() = {root = Leaf}
 
 let push(value, tree) =
   let rec innerPush = function
-    Node(v, left, right) as tr -> if value < v then Node(v, innerPush left, right)
-                                   else if value > v then Node(v, left, innerPush right)
-                                   else tr
-   |Leaf -> Node(value, Leaf, Leaf)
+      Node(v, left, right) as tr -> if value < v then Node(v, innerPush left, right)
+        else if value > v then Node(v, left, innerPush right)
+        else tr
+    |Leaf -> Node(value, Leaf, Leaf)
   in 
-  tree.root <- innerPush tree.root
+    tree.root <- innerPush tree.root
 
 let find(value, tree) = 
   let rec inFind = function
-    Node(v, left, right) -> if value < v then inFind left
-                            else if value > v then inFind right
-                            else true
-   |Leaf -> false
+      Node(v, left, right) -> if value < v then inFind left
+        else if value > v then inFind right
+        else true
+    |Leaf -> false
   in 
-  inFind tree.root
+    inFind tree.root
+
+let remove(value, tree) = 
+  let rec inRemove inVal = function
+      Node(v, left, right) as removed -> if inVal < v then Node(v, inRemove inVal left, right)
+        else if inVal > v then Node(v, left, inRemove inVal right)
+        else (match removed with
+               |Node(_, left, Leaf) -> left
+               |Node(_, Leaf, right) -> right
+               |Node(_, Leaf, Leaf) -> Leaf
+               |Leaf -> failwith "No such value contained"
+               |Node(_, left, right) -> 
+                   let rec findMinimum = function
+                     |Node(minimum, Leaf, _) -> minimum
+                     |Node(_, left, _) -> findMinimum left
+                     |Leaf -> failwith "Unable to find successor"
+                   in let successor = findMinimum right
+                   in Node(successor, left, inRemove successor right)
+             )
+    |Leaf -> raise (NotFound "bst: remove")
+  in 
+    tree.root <- inRemove value tree.root
+
 
 
 let getPreOrder tree =
   let rec preOrder = function
-    Node(v, left, right) -> v :: preOrder left @ preOrder right
+      Node(v, left, right) -> v :: preOrder left @ preOrder right
     |Leaf -> []
   in
-  preOrder tree.root
+    preOrder tree.root
 
-  let getPostOrder tree =
-    let rec postOrder = function
+let getPostOrder tree =
+  let rec postOrder = function
       Node(v, left, right) ->  postOrder left @ postOrder right @ [v]
-      |Leaf -> []
-    in
+    |Leaf -> []
+  in
     postOrder tree.root
 
-    let getInOrder tree =
-      let rec inOrder = function
-        Node(v, left, right) ->  inOrder left @ (v :: inOrder right) 
-        |Leaf -> []
-      in
-      inOrder tree.root
-  
-  
+let getInOrder tree =
+  let rec inOrder = function
+      Node(v, left, right) ->  inOrder left @ (v :: inOrder right) 
+    |Leaf -> []
+  in
+    inOrder tree.root
 
 
 
--- Lab_Lists/list7/bst.mli
+++ Lab_Lists/list7/bst.mli
@@ -1,11 +1,11 @@
 
-    type 'a tr
-    val create: unit -> int tr
-    val push: int * int tr -> unit
-    val remove: int * int tr -> unit
-    val find: int * int tr -> bool
-    val getPreOrder: int tr -> int list
-    val getPostOrder: int tr -> int list
-    val getInOrder: int tr -> int list
-  
+type 'a tr
+exception NotFound of string
+val create: unit -> int tr
+val push: int * int tr -> unit
+val remove: int * int tr -> unit
+val find: int * int tr -> bool
+val getPreOrder: int tr -> int list
+val getPostOrder: int tr -> int list
+val getInOrder: int tr -> int list
 
