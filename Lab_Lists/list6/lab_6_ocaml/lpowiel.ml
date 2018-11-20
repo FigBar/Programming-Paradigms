a6f03fa981e0494136a991c5cdef7120e021c20d
Bartosz <bartekfigiel98@gmail.com>
2018-11-20 20:00:30 +0100

    Fixed the case with finished lazy list in lpowiel

--- Lab_Lists/list6/lab_6_ocaml/lpowiel.ml
+++ Lab_Lists/list6/lab_6_ocaml/lpowiel.ml
@@ -8,16 +8,25 @@ let lhd = function
 let lrepeat llist = 
   let rec innerRepeat n = function
     LNil -> LNil
-   |LCons(x, fx) as list -> if n = 0 then innerRepeat(lhd(fx()))(fx())
+   |LCons(x, tl) as list -> match tl() with
+      LNil -> if n = 0 then innerRepeat(x)(LNil)
+              else LCons(x, fun() -> innerRepeat(n - 1) list)
+     |LCons(_, _) -> if n = 0 then innerRepeat(lhd(tl()))(tl())
                     else LCons(x, fun() -> innerRepeat(n - 1) list)
   in innerRepeat (lhd llist) llist;;
   
-let rec ltake = function
+let rec take = function
 (0, _) -> []
 |(_, LNil) -> []
-|(n, LCons(x, xf)) -> x::ltake(n-1, xf());;
+|(n, LCons(x, xf)) -> x::take(n-1, xf());;
+
+let rec ltake = function
+(0, _) -> LNil
+|(_, LNil) -> LNil
+|(n, LCons(x, xf)) -> LCons(x, fun() -> ltake(n-1, xf()));;
 
 let rec lfrom k = LCons (k, function() -> lfrom(k+1));;
 
 
-ltake(10, lrepeat (lfrom 2));;
+take(10, lrepeat (lfrom 2));;
+take(20, lrepeat(ltake (3, lfrom 3)));;
