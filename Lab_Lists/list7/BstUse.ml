10684577e6ebc831c2c95f87b7804804efb7d95a
Bartosz <bartekfigiel98@gmail.com>
2018-11-28 15:41:51 +0100

    Added more test cases to the binary search tree test

--- Lab_Lists/list7/BstUse.ml
+++ Lab_Lists/list7/BstUse.ml
@@ -19,6 +19,23 @@ Bst.push(2, tree);;
 Bst.push(5, tree);;
 Bst.push(4, tree);;
 print_string "Added 3, 2, 5, 4 to the tree\n";;
+
+print_string "If contains 4: ";;
+print_string(string_of_bool(Bst.find(4,tree)));;
+print_string "\n";;
+
+print_string "If contains 2: ";;
+print_string(string_of_bool(Bst.find(2,tree)));;
+print_string "\n";;
+
+print_string "If contains 5: ";;
+print_string(string_of_bool(Bst.find(5,tree)));;
+print_string "\n";;
+
+print_string "If contains 10: ";;
+print_string(string_of_bool(Bst.find(10,tree)));;
+print_string "\n";;
+
 print_string "Inorder: ";;
 printList(Bst.getInOrder tree);;
 print_string "\n";;
@@ -47,6 +64,50 @@ print_string "\n";;
 print_string "Inorder after deleting 3: ";;
 printList(Bst.getInOrder tree);;
 print_string "\n";;
+
+Bst.push(10, tree);;
+Bst.push(0, tree);;
+
+print_string "Added 10, 0 to the tree\n";;
+
+print_string "If contains 10: ";;
+print_string(string_of_bool(Bst.find(10,tree)));;
+print_string "\n";;
+
+print_string "If contains 0: ";;
+print_string(string_of_bool(Bst.find(0,tree)));;
+print_string "\n";;
+
+
+print_string "Inorder: ";;
+printList(Bst.getInOrder tree);;
+print_string "\n";;
+
+print_string "Preorder: ";;
+printList(Bst.getPreOrder tree);;
+print_string "\n";;
+
+print_string "Postorder: ";;
+printList(Bst.getPostOrder tree);;
+print_string "\n";;
+
+
+print_string "Deleting all elements";;
+print_string "\n";;
+Bst.remove(0,tree);;
+Bst.remove(4,tree);;
+Bst.remove(5,tree);;
+Bst.remove(2,tree);;
+Bst.remove(10,tree);;
+
+print_string "Inorder: ";;
+printList(Bst.getInOrder tree);;
+print_string "\n";;
+
+
+
+
+
 
 
 
